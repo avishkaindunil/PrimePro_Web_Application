eae011cafce37f089f1c7af3cf34364d60f75889
avishkaindunil <haavishkaindunil@gmail.com>
2024-07-25 21:49:43 +0530

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './components/Sidebar.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import {MainDashboard } from '../pages'
+import MainDashboard from './pages/MainDashboard';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
-function App() {
-
+export default function App() {
   return (
-    <>
-   <h1 className="text-3xl font-bold bg-red-500 underline">
-      Hello world!
-    </h1>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<MainDashboard />} />
+          <Route path="/maindashboard" element={<MainDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
-export default App
+// function AppOne() {
+
+//   return (
+//     <>
+//    <h1 className="text-3xl font-bold bg-red-500 underline">
+//       Hello world!
+//     </h1>
+//     </>
+//   )
+// }
+
+
---
+++ src/pages/MainDashboard.jsx
@@ -0,0 +1,10 @@
+import Sidebar from '../components/Sidebar';
+
+export default function MainDashboard() {
+    return (
+        <>
+            <Sidebar />
+            <h2>Main Dashboard</h2>
+        </>
+    )
+}
