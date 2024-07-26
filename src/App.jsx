2826f6cec1216912400e4b53d0f1e57e62113868
shanikaayasmanthi <2021cs016@stu.ucsc.cmb.ac.lk>
2024-07-26 11:13:40 +0530

    created the main layout and add routs

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,36 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AppHeader from './Components/Header'
+import SideBar from './Components/SideBar'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import CenterAdminRoutes from './Routes/CenterAdminRoutes'
+import EmployeeRoutes from './Routes/EmployeeRoutes'
+import MainLayout from './Components/MainLayout'
+import Dashboard from './Pages/CenterAdmin/Dashboard'
+
+
+const userType ='CenterAdmin';
 
 function App() {
 
   return (
     <>
-   <h1 className="text-3xl font-bold bg-red-500 underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      
+        <Routes>
+          <Route path="/" element={<MainLayout userType={userType}/>}>
+              {userType ==="CenterAdmin" &&<Route path='/*' element={<CenterAdminRoutes/>}/>}
+              {userType ==="Employee" && <Route path='/*' element={<EmployeeRoutes/>}/>}
+            </Route>
+        </Routes>
+    
+    </BrowserRouter>
     </>
-  )
-}
+         
+    
+    
+  );
+};
 
 export default App
