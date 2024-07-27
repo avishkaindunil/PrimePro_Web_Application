c94014ff956dd6dda12727485d2e654634d26313
binalitanya <binaliukwatte@gmail.com>
2024-07-28 01:47:31 +0530

    task/add dashboard boxes

---
+++ src/components/Employee/DashboardBoxes.jsx
@@ -0,0 +1,63 @@
+import PropTypes from "prop-types";
+import {
+  faTachometerAlt,
+  faCalendarAlt,
+  faUsers,
+  faTasks,
+  faChartBar,
+  faHeadset,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function getIcon(iconName) {
+  switch (iconName) {
+    case "faChartBar":
+      return faChartBar;
+    case "faTasks":
+      return faTasks;
+    case "faCalendarAlt":
+      return faCalendarAlt;
+    default:
+      return null;
+  }
+}
+const DashboardBoxes = ({ content }) => {
+  return (
+    <div className="flex gap-16 items-center justify-center cursor-pointer px-8">
+      {content.map((item, index) => (
+        <div
+          key={index}
+          style={{ backgroundColor: item.color, borderColor: item.color }}
+          className="flex-1 border my-12 p-4 rounded-lg shadow-md"
+        >
+          <div className="text-2xl text-center py-4">{item.title}</div>
+          <div className="flex justify-evenly py-4 text-2xl items-center">
+            {/* <div className="text-3xl font-medium bg-white py-2 px-4 rounded-lg">{item.value}</div> */}
+            <div className="text-3xl font-medium">{item.value}</div>
+            <div className="">
+              <FontAwesomeIcon
+                icon={getIcon(item.icon)}
+                style={{ backgroundColor: item.iconColor }}
+                className="text-2xl p-4 text-white rounded-full z-30"
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+// Define the prop types
+DashboardBoxes.propTypes = {
+  content: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      icon: PropTypes.node.isRequired, // Assuming icon is a React node
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+        .isRequired,
+    })
+  ).isRequired,
+};
+
+export default DashboardBoxes;
--- src/pages/Employee/MainDashboard.jsx
+++ src/pages/Employee/MainDashboard.jsx
@@ -1,11 +1,35 @@
+import DashboardBoxes from "../../components/Employee/DashboardBoxes";
 import DateAndTimeTracker from "../../components/Employee/DateAndTimeTracker";
 
-export default function MainDashboard() {
-  
+const boxContent = [
+  {
+    title: "Total Tasks Completed",
+    value: "18",
+    icon: "faTasks",
+    color: "rgba(5, 0, 255, 0.17)",
+    iconColor: "#2948AB"
+  },
+  {
+    title: "Total Salary",
+    value: "LKR 25000",
+    icon: "faChartBar",
+    color: "#4ADEDE",
+    iconColor: "#02A8B0"
+  },
+  {
+    title: "Days Worked",
+    value: "18",
+    icon: "faCalendarAlt",
+    color: "rgba(191, 121, 246, 0.40)",
+    iconColor: "#2338EE"
+  },
+];
 
+export default function MainDashboard() {
   return (
     <>
       <DateAndTimeTracker />
+      <DashboardBoxes content={boxContent} />
     </>
   );
 }
