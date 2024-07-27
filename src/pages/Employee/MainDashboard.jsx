3e9a26393130cfae7d5d712147e10788060ca0ba
binalitanya <binaliukwatte@gmail.com>
2024-07-27 12:14:29 +0530

    task/fixed overflow issue

--- src/pages/Employee/MainDashboard.jsx
+++ src/pages/Employee/MainDashboard.jsx
@@ -1,9 +1,15 @@
 export default function MainDashboard() {
     return (
         <>
-            <h2 className='text-black z-50'>Employee Main Dashboard</h2>
+            {/* <h2 className='text-black z-50'>Employee Main Dashboard</h2> */}
 
-            <div className='flex flex-wrap gap-5'>
+            <div className='flex gap-5'>
+                <div className='w-1/2'>
+                    <div className='bg-white rounded-lg shadow-md p-5'>
+                        <h3 className='text-gray-700 text-xl'>Employee Details</h3>
+                        {/* Employee details */}
+                    </div>
+                </div>
                 <div className='w-1/2'>
                     <div className='bg-white rounded-lg shadow-md p-5'>
                         <h3 className='text-gray-700 text-xl'>Employee Details</h3>
