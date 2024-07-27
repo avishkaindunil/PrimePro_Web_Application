73e01c5adeafacb258b5d24e0730c3191f42ac56
binalitanya <binaliukwatte@gmail.com>
2024-07-27 12:36:19 +0530

    Task/add time tracker component

--- src/pages/Employee/MainDashboard.jsx
+++ src/pages/Employee/MainDashboard.jsx
@@ -1,22 +1,11 @@
-export default function MainDashboard() {
-    return (
-        <>
-            {/* <h2 className='text-black z-50'>Employee Main Dashboard</h2> */}
+import DateAndTimeTracker from "../../components/Employee/DateAndTimeTracker";
 
-            <div className='flex gap-5'>
-                <div className='w-1/2'>
-                    <div className='bg-white rounded-lg shadow-md p-5'>
-                        <h3 className='text-gray-700 text-xl'>Employee Details</h3>
-                        {/* Employee details */}
-                    </div>
-                </div>
-                <div className='w-1/2'>
-                    <div className='bg-white rounded-lg shadow-md p-5'>
-                        <h3 className='text-gray-700 text-xl'>Employee Details</h3>
-                        {/* Employee details */}
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+export default function MainDashboard() {
+  
+
+  return (
+    <>
+      <DateAndTimeTracker />
+    </>
+  );
 }
