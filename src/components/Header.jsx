f2d0778c5fb48fab114236c97dfd4aa1b0926ef7
binalitanya <binaliukwatte@gmail.com>
2024-07-31 20:58:31 +0530

    task/add QR code generator

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,15 @@ import { faBell, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import EmployeeQRCode from "./Employee/EmployeeQRCode";
+import { userTypes } from "./Constants";
 
 const AppHeader = () => {
   const navigate = useNavigate();
+  const storedUserData = localStorage.getItem("userData");
+
+  const user = storedUserData ? JSON.parse(storedUserData) : null;
+
   const handleIconClick = () => {
     Swal.fire({
       title: "Are you sure you want to log out?",
@@ -26,6 +32,7 @@ const AppHeader = () => {
     <div className="w-full">
       {/* <header className="h-6 bg-black"></header> */}
       <div className="flex items-center justify-end mr-8 bg-white h-[55px]">
+        {user?.role == userTypes.EMPLOYEE && <EmployeeQRCode employeeId={1} />}
         <FontAwesomeIcon className="text-xl" icon={faBell} />
         <FontAwesomeIcon
           className="text-xl px-4"
